cd6cc0c128f666e37d0694ef1dc5e6eaedde4b28
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-07-14 22:35:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
-require('./models/User');
-require('./models/Blog');
-require('./services/passport');
+require("./models/User");
+require("./models/Blog");
+require("./services/passport");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
@@ -27,19 +27,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/blogRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/blogRoutes")(app);
 
-if (['production'].includes(process.env.NODE_ENV)) {
-  app.use(express.static('client/build'));
+if (["production"].includes(process.env.NODE_ENV)) {
+  app.use(express.static("client/build"));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("client", "build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
