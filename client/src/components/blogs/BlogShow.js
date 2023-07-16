659312b160e6846b7a0d5f8ea7c1514d35a67189
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-07-17 04:37:55 +0900

    img alt compile error fix

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -15,7 +15,7 @@ class BlogShow extends Component {
             "https://my-bucket-byun.s3.ap-northeast-2.amazonaws.com/" +
             this.props.blog.imageUrl
           }
-          alt="Served Image"
+          alt="Uploaded file served by AWS S3"
         />
       );
     }
